4eb81ad7608008213797edab219c8bd7da4fd499
Disarmy <rlarns0412@naver.com>
2022-01-11 17:43:49 +0900

    Update level2 Ddangddaemokgi.java

---
+++ programmers_level2/src/Ddangddaemokgi.java
@@ -0,0 +1,24 @@
+public class Ddangddaemokgi {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[][]{{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}}));
+    }
+
+    public static int solution(int[][] land) {
+        int answer = 0;
+
+        //이전의 행에서 가장 큰 수를 더함
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(Math.max(land[i - 1][1], land[i - 1][2]), land[i - 1][3]);
+            land[i][1] += Math.max(Math.max(land[i - 1][0], land[i - 1][2]), land[i - 1][3]);
+            land[i][2] += Math.max(Math.max(land[i - 1][1], land[i - 1][0]), land[i - 1][3]);
+            land[i][3] += Math.max(Math.max(land[i - 1][1], land[i - 1][2]), land[i - 1][0]);
+        }
+
+        //가장 마지막 행에서 제일 큰 수를 구함
+        for (int max : land[land.length - 1]) {
+            answer = Math.max(answer, max);
+        }
+
+        return answer;
+    }
+}
