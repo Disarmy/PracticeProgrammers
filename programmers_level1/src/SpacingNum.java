2a2960acdd00ea3b4adfa4d73370f44bd2680c0b
Disarmy <rlarns0412@naver.com>
2022-01-22 17:23:13 +0900

    Update level1 SpacingNum.java

---
+++ programmers_level1/src/SpacingNum.java
@@ -0,0 +1,12 @@
+public class SpacingNum {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = x;
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = num * (i + 1);
+        }
+
+        return answer;
+    }
+}
