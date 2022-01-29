74979bf4c50b705847dd4f17eacc003699cc0442
Disarmy <rlarns0412@naver.com>
2022-01-29 15:10:39 +0900

    Update level1 AddDigits.java

---
+++ programmers_level1/src/AddDigits.java
@@ -0,0 +1,13 @@
+public class AddDigits {
+    public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            int num = n % 10;
+            answer += num;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
