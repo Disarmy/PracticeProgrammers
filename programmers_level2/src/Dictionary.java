8c74d73e96e5038336a9a98fe247d984de550e6e
Disarmy <rlarns0412@naver.com>
2022-02-11 19:53:38 +0900

    Update level2 Dictionary.java

--- programmers_level2/src/Dictionary.java
+++ programmers_level2/src/Dictionary.java
@@ -10,11 +10,11 @@ public class Dictionary {
         //A E I O U
         for(int i = 1; i <= word.length(); i++){
             switch (word.charAt(i - 1)) {
-                case 'A' -> answer += 1;
-                case 'E' -> answer += ((max / Math.pow(5, i) * 1)) + 1;
-                case 'I' -> answer += ((max / Math.pow(5, i) * 2)) + 1;
-                case 'O' -> answer += ((max / Math.pow(5, i) * 3)) + 1;
-                case 'U' -> answer += ((max / Math.pow(5, i) * 4)) + 1;
+                case 'A' : answer += 1; break;
+                case 'E' : answer += ((max / Math.pow(5, i) * 1)) + 1; break;
+                case 'I' : answer += ((max / Math.pow(5, i) * 2)) + 1; break;
+                case 'O' : answer += ((max / Math.pow(5, i) * 3)) + 1; break;
+                case 'U' : answer += ((max / Math.pow(5, i) * 4)) + 1; break;
             }
         }
 
