15c6d6f84e0b0b880c45c420fb874ac7a6405c87
Disarmy <rlarns0412@naver.com>
2022-02-14 17:52:46 +0900

    Update level2 ExpectationMatches.java

---
+++ programmers_level2/src/ExpectationMatches.java
@@ -0,0 +1,18 @@
+public class ExpectationMatches {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        while (a != b) {
+            answer++;
+            if (a % 2 == 1)
+                a += 1;
+            if (b % 2 == 1)
+                b += 1;
+
+            a /= 2;
+            b /= 2;
+        }
+
+        return answer;
+    }
+}
